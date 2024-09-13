8a7bd6051e7f2cec9cbb85cec0d1c3bff1bec57d
Aurelien St <aurelien.strimelle@bstorm.be>
2024-09-13 16:07:02 +0200

    Traitement des formulaires

--- Demo_ASPMVC_02/Controllers/AuthController.cs
+++ Demo_ASPMVC_02/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo_ASPMVC_02.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_ASPMVC_02.Controllers
 {
@@ -9,9 +10,41 @@ namespace Demo_ASPMVC_02.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login([FromForm] AuthLoginFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Connexion de l'utilisateur via la session
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromForm] AuthRegisterFormModel model)
+        {
+            if(model.BirthDate is not null && model.BirthDate >= DateTime.Today.AddYears(-12))
+            {
+                ModelState.AddModelError(nameof(AuthRegisterFormModel.BirthDate), "T'as po 12 ans o((>ω< ))o");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Création d'un compte dans l'application
+
+            return RedirectToAction(nameof(Login));
         }
 
         public IActionResult Logout()
